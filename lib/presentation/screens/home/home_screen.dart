1ef416b3a61527d80c941588f7cda92758522d74
Admin <phucnguyen.mobiledeveloper@gmail.com>
2024-02-21 13:55:57 +0700

    update home ui.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -18,29 +18,34 @@ class HomeScreen extends StatelessWidget {
             titleSpacing: 16.0,
             backgroundColor: Colors.green.shade700,
             elevation: 0.0,
-            title: Container(
-              width: double.infinity,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(4.0),
-              ),
-              child: const Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.search,
-                    size: 18.0,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    'Tìm kiếm quán ăn, nhà hàng,...',
-                    style: TextStyle(fontSize: 14.0, color: Colors.grey),
-                  ),
-                ],
+            title: InkWell(
+              onTap: () {
+                showSearch(context: context, delegate: RestaurantDelegate());
+              },
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 14.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: 18.0,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      'Tìm kiếm quán ăn, nhà hàng,...',
+                      style: TextStyle(fontSize: 14.0, color: Colors.grey),
+                    ),
+                  ],
+                ),
               ),
             ),
             bottom: const TabBar(
@@ -63,6 +68,51 @@ class HomeScreen extends StatelessWidget {
                 KaraList(),
                 HotelView(),
               ])),
+    );
+  }
+}
+
+class RestaurantDelegate extends SearchDelegate {
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(elevation: 0.0),
+      inputDecorationTheme: InputDecorationTheme(
+        border: InputBorder.none,
+      )
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.close),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Column(
+      children: [],
     );
   }
 }
